cb2c41a384945c3a8c166673236ce1fc535afd01
esmaelhussen <esmaelhussenabdu@gmail.com>
2024-11-14 10:22:16 +0300

    update my files

--- webpack.config.js
+++ webpack.config.js
@@ -4,32 +4,33 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: {
-    index:'./src/index.js',
+    index: './src/index.js', // Entry point for your app
   },
-  devtool:'inline-source-map',
+  devtool: 'inline-source-map',
   devServer: {
-    static: "./dist",
+    static: "./dist", // Serve static files from the 'dist' folder
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: "./src/index.html", // Template HTML file
+      base: "/To-do-list/", // Ensure correct resolution of relative paths
     }),
   ],
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-    publicPath: "/",
+    filename: "[name].bundle.js", // Output bundled file
+    path: path.resolve(__dirname, "dist"), // Output folder for bundles
+    clean: true, // Clean the output folder before each build
+    publicPath: "/To-do-list/", // Update this for GitHub Pages
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ["style-loader", "css-loader"], // Handle CSS files
       },
     ],
   },
   optimization: {
-   runtimeChunk:'single',
-    },
+    runtimeChunk: 'single', // Extract runtime chunk
+  },
 };
