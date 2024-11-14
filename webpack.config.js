dfbab6df830aa86220e7c7deed50b0a735bcd7e5
esmaelhussen <esmaelhussenabdu@gmail.com>
2024-11-14 10:38:48 +0300

    add

--- webpack.config.js
+++ webpack.config.js
@@ -4,33 +4,32 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: {
-    index: './src/index.js', // Entry point for your app
+    index:'./src/index.js',
   },
-  devtool: 'inline-source-map',
+  devtool:'inline-source-map',
   devServer: {
-    static: "./dist", // Serve static files from the 'dist' folder
+    static: "./dist",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html", // Template HTML file
-      base: "/To-do-list/", // Ensure correct resolution of relative paths
+      template: "./src/index.html",
     }),
   ],
   output: {
-    filename: "[name].bundle.js", // Output bundled file
-    path: path.resolve(__dirname, "dist"), // Output folder for bundles
-    clean: true, // Clean the output folder before each build
-    publicPath: "/To-do-list/", // Update this for GitHub Pages
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    publicPath: "/",
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"], // Handle CSS files
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
   optimization: {
-    runtimeChunk: 'single', // Extract runtime chunk
-  },
+   runtimeChunk:'single'
+    },
 };
